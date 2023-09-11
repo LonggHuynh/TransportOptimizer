6c19d47418c7432d240dcba5ac9c3231ef004523
Long Huynh <long.huynh5713@gmail.com>
2023-09-11 20:19:35 +0300

    Added go backend.

---
+++ frontend/src/components/PlaceTag.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import CloseIcon from '@mui/icons-material/Close';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import './PlaceTag.css';
+
+const PlaceTag = ({ name, removeItem, locatePlace }) => {
+    return (
+        <div className="place-tag-container">
+            <div>{name}</div>
+            <button type="button" onClick={locatePlace}>
+                <LocationOnIcon />
+            </button>
+            <button
+                className="remove-button"
+                type="button"
+                onClick={removeItem}
+            >
+                <CloseIcon />
+            </button>
+        </div>
+    );
+};
+
+export default PlaceTag;
