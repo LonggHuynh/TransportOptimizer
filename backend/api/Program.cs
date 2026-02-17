b7b5370e204ebfb80e7f023d17eb99e6c419ee65
Long Huynh <long.huynh5713@gmail.com>
2026-02-17 08:59:30 +0000

    Fix backend host access in devcontainer

--- backend/api/Program.cs
+++ backend/api/Program.cs
@@ -162,7 +162,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
 
 app.UseCors("CorsPolicy");
 
